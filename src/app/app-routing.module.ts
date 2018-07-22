20e8a690c8062ead4123ad022ef58788f4ff4727
dianapelaez <peldiana@gmail.com>
2018-07-22 11:43:02 -0500

    My project is working, I have created my routing module and my user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/appComponent', pathMatch: 'full'},
+  {path: 'appComponent', component: AppComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
